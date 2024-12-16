e11dd4a4056180ef078136d9d907b136561a743d
ashan24019 <aigavalakolanga@std.appsc.sab.ac.lk>
2024-12-16 11:33:40 +0530

    Snake Game detect collusion with food

--- Snake Game/food.py
+++ Snake Game/food.py
@@ -3,14 +3,16 @@ import random
 
 
 class Food(Turtle):
-    
-    def __int__(self):
+    def __init__(self):
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(stretch.len=10.5, strtch.wid=0.5)
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
-        self.speed("faster")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
-        self.goto()
+        self.goto(random_x, random_y)
--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,5 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,8 +22,9 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    snake.move()
 
-    snake.move() 
-
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
