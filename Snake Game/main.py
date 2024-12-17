fb0ee8a86b1fcd4c8062e45b9c3ee57ac6df8066
ashan24019 <aigavalakolanga@std.appsc.sab.ac.lk>
2024-12-17 21:39:55 +0530

    Detect collusion with wall

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -26,8 +26,14 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # Detect collusion with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+
+    # Detect collusion with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
