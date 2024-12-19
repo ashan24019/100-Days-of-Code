da5432cbd2b4995318999aaa870e3efc3fdc110e
ashan24019 <aigavalakolanga@std.appsc.sab.ac.lk>
2024-12-19 23:56:02 +0530

    Create a paddle that responds to key presses

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,8 +1,36 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=600, height=600)
 screen.title("Pong")
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5, stretch_len=10)
+paddle.color("white")
+paddle.penup()
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "up")
+screen.onkey(go_down, "down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 
 screen.exitonclick()
