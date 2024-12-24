20d95b66a434a78736c4365320e9f5b728618134
ashan24019 <aigavalakolanga@std.appsc.sab.ac.lk>
2024-12-24 15:53:56 +0530

    Create a ball and make the ball move

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,14 +1,17 @@
 from turtle import Screen, Turtle
-from paddle import  Paddle
+from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=600, height=600)
+screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 
 screen.listen()
@@ -19,7 +22,10 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
 
 
 screen.exitonclick()
--- Day 22/paddle.py
+++ Day 22/paddle.py
@@ -5,7 +5,7 @@ class Paddle(Turtle,):
     def __init__(self, position):
         super().__init__()
         self.shape("square")
-        self.shapesize(stretch_wid=5, stretch_len=8)
+        self.shapesize(stretch_wid=5, stretch_len=1)
         self.color("white")
         self.penup()
         self.goto(position)
