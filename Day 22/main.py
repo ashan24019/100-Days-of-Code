cd1767eabc3b3b0a42294ef594395344bc7ce58d
ashan24019 <aigavalakolanga@std.appsc.sab.ac.lk>
2024-12-24 16:50:18 +0530

    improve ball to bounce when hit the edge of the y

--- Day 22/main.py
+++ Day 22/main.py
@@ -26,6 +26,12 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+        # ball needs to bounce
+
 
 
 screen.exitonclick()
