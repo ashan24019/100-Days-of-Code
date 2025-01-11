bb5b89c125b8d01a46b9f266484066fb486c62c3
ashan24019 <aigavalakolanga@std.appsc.sab.ac.lk>
2025-01-12 03:57:52 +0530

    Data frames and series

--- Day 25/main.py
+++ Day 25/main.py
@@ -17,5 +17,26 @@ import pandas
 
 data = pandas.read_csv("weather-data.csv")
 
-print(data["temp"])
+# print(data["temp"])
+#
+# data_dict = data.temp.to_dict()
+# print(data_dict)
+#
+# average = sum(data["temp"]) / len(data["temp"])
+#
+#
+# print(f"Average temperature: {round(average)}")
+#
+# print(round(data["temp"].mean()))
+
+max_num = max(data.temp)
+
+print(data[data.temp == max_num])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+temp_in_f = monday.temp + 273
+
+print(temp_in_f)
 
