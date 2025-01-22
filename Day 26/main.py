b5d4301308e37e952d6d02b7d8fb92ae149834ef
ashan24019 <aigavalakolanga@std.appsc.sab.ac.lk>
2025-01-22 19:12:56 +0530

    Dictionalry comprehension activity 2

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,9 +1,23 @@
 
-sentence = "What is the Airspeed velocity of an Unladen Swallow?"
+# sentence = "What is the Airspeed velocity of an Unladen Swallow?"
+#
+# word_list = sentence.split()
+# print(word_list)
+#
+# word_and_letter_count = {word:len(word) for word in word_list}
+#
+# print(word_and_letter_count)
 
-word_list = sentence.split()
-print(word_list)
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
 
-word_and_letter_count = {word:len(word) for word in word_list}
+weather_f = {day: temp*9/5 + 32 for (day, temp) in weather_c.items()}
 
-print(word_and_letter_count)
+print(weather_f)
