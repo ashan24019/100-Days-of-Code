b347b67eab551ed6423a297230700c9738d46b7e
Ashan <ashanvimodh241@gmail.com>
2025-02-02 09:44:12 +0530

    Day 27 Complete with proper variable names

--- day-27/Mile to Km Converter.py
+++ day-27/Mile to Km Converter.py
@@ -7,24 +7,24 @@ window.title("Mile to Km Converter")
 window.minsize(width=600, height=300)
 window.config(padx=100, pady=50)
 
-input_box = Entry(width=10)
-input_box.grid(column=1, row=0)
+miles_input = Entry(width=10)
+miles_input.grid(column=1, row=0)
 
-miles = Label(text = "Miles")
-miles.grid(column=2, row=0)
+miles_label = Label(text ="Miles")
+miles_label.grid(column=2, row=0)
 
-km = Label(text = "Km")
-km.grid(column=2, row=1)
+kilometer_label = Label(text ="Km")
+kilometer_label.grid(column=2, row=1)
 
-equal_to = Label(text = "is equal to")
-equal_to.grid(column=0, row=1)
+is_equal_to = Label(text ="is equal to")
+is_equal_to.grid(column=0, row=1)
 
 def calculator():
-    answer = int(input_box.get()) * 1.6
+    answer = int(miles_input.get()) * 1.6
     new_text.config(text=answer)
 
-button = Button(text = "Calculate", command=calculator)
-button.grid(column=1, row=2)
+calculate_button = Button(text ="Calculate", command=calculator)
+calculate_button.grid(column=1, row=2)
 
 new_text = Label(text= 0)
 new_text.grid(column=1, row=1)
