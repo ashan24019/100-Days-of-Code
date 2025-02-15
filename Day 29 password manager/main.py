81ccb34d2a5862d9abd2d770a363368f0c4fdc24
Ashan <ashanvimodh241@gmail.com>
2025-02-15 08:20:21 +0530

    Write, read, update JSON data

--- Day 29 password manager/main.py
+++ Day 29 password manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice,randint,shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -29,20 +30,29 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website:{
+            "email": email,
+            "password": password,
+        }
+    }
+
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title=website, message="Please make sure you haven't left any fields empty")
-
     else:
+        with open("data.json", "r") as data_file:
+            # Reading old data
+            data = json.load(data_file)
+            # updating old data
+            data.update(new_data)
 
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
-                               f"\nPassword: {password}\nIs it ok to save?")
+        with open("data.json", "w") as data_file:
+            # Saving updating data
+            json.dump(data, data_file, indent=4)
 
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
