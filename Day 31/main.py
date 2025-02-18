ca239196ca9ed7a15a2ad802dc413dc1cdd16504
Ashan <ashanvimodh241@gmail.com>
2025-02-18 10:42:54 +0530

    UI setup

---
+++ Day 31/main.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import pandas
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict()
+
+def next_card():
+    pass
+
+window = Tk()
+window.title("Flashy")
+window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+card_front_image = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_image)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button.grid(row=1, column=1)
+
+
+window.mainloop()
+
+
