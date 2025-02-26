57eea7b733514fe76d16ad6ada2eb520551aaf33
Ashan <ashanvimodh241@gmail.com>
2025-02-26 16:10:47 +0530

    Working with date time module

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,13 +1,24 @@
-import smtplib
+# import smtplib
+#
+# my_email = "ashanvimodh@yahoo.com"
+# password = "Dr.rossgellerPHD"
+#
+# with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="testingashan@gmail.com",
+#         msg="Subject:Hello\n\nThis is he body of my email"
+#     )
 
-my_email = "ashanvimodh@yahoo.com"
-password = "Dr.rossgellerPHD"
+import datetime as dt
 
-with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="testingashan@gmail.com",
-        msg="Subject:Hello\n\nThis is he body of my email"
-    )
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=2001, month=3, day=30)
+print(date_of_birth)
