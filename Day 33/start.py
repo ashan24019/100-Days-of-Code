c255a559a284f2dda11f2539dec6e0ec0747a15c
Ashan <ashanvimodh241@gmail.com>
2025-03-01 17:23:09 +0530

    Match the sunset times with the current time

--- Day 33/start.py
+++ Day 33/start.py
@@ -1,11 +1,39 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 6.719772
+MY_LONG = 80.771948
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+
+
+parameters = {
+    "lat":MY_LAT,
+    "lng":MY_LONG,
+    "formatted":0,
+}
+
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+print(sunrise)
+print(sunset)
 
-iss_position = (longitude, latitude)
+time_now = datetime.now()
+print(time_now.hour)
+
+
+
+
