ecd26ca49218e13fdd9f7365f007d3e50524b9a0
Ashan <ashanvimodh241@gmail.com>
2025-03-06 17:49:01 +0530

    Unescaping HTML entities

--- Day 34 Quizer App/data.py
+++ Day 34 Quizer App/data.py
@@ -5,10 +5,10 @@ parameters = {
     "type":"boolean"
 }
 
-requests.get("https://opentdb.com/api.php", params=parameters)
+response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+question_data = data["results"]
 
 
 
