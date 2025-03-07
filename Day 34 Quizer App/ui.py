8c94f964bfcc52b46a1a58ed0b1b2674855c90b8
Ashan <ashanvimodh241@gmail.com>
2025-03-07 14:46:34 +0530

    Typing and showing next question in the GUI

--- Day 34 Quizer App/ui.py
+++ Day 34 Quizer App/ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain:QuizBrain ):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzer")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -16,6 +18,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="Some Question Text",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
@@ -27,11 +30,15 @@ class QuizInterface:
         self.true_button.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button = Button(image=false_image, highlightthickness=0)
         self.true_button.grid(row=2, column=1)
 
-        self.right_mark = Label()
+        self.get_next_question()
 
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
 
