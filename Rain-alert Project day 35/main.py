0d8d85e5456956191c152e416446249f19454499
Ashan <ashanvimodh241@gmail.com>
2025-03-09 09:11:40 +0530

    Check if rain in next 12 hours

--- Rain-alert Project day 35/main.py
+++ Rain-alert Project day 35/main.py
@@ -1,14 +1,28 @@
 import requests
 
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "3046154e9fae10a676cb5586084292a4"
+
 parameters = {
     "lat" : 6.705574,
     "lon" : 80.384735,
-    "appid" : "3046154e9fae10a676cb5586084292a4"
+    "appid" : api_key,
+    "cnt": 4,
 }
 
-
-response = requests.get("https://api.openweathermap.org/data/2.5/weather", params=parameters)
+response = requests.get(OWM_Endpoint, params=parameters)
 response.raise_for_status()
-data = response.json()
-print(data)
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0])
+
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
+
 
